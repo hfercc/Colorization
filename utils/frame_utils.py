d227bac7b6c254784b98a594ecd984fea4066df1
hfercc <hfercxx@hotmail.com>
2019-09-22 09:30:34 +0800

    p

--- utils/frame_utils.py
+++ utils/frame_utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from os.path import *
-from scipy.misc import imread
+from scipy.ndimage import imread
 gray_sub = imread('/home/chuchienshu/Downloads/dataset/DAVIS_test/boxing/00002.jpg')
 def read_gen(file_name):
     ext = splitext(file_name)[-1].lower()
