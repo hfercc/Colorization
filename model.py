73ce5360616aa5af710baa61634e7cf65262cdd6
hfercc <hfercxx@hotmail.com>
2019-09-28 20:54:51 +0800

    p

--- model.py
+++ model.py
@@ -195,6 +195,7 @@ class ColorizationResNet(nn.Module):
         
     def forward(self, gt_img):
         gt_img_l = (gt_img[:,:1,:,:] - 50.) * 0.02
+        print(x.shape)
         x = self.relu (self.main(gt_img_l))
         print(x.shape)
         x = self.conv_8(x)
